be17124487d1ad43baba6ff5ba20ec419e44ccac
John Stewart <jnicklausstewart@yahoo.com>
2020-03-30 14:40:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,3 +15,9 @@ def retrieval
   shipping_manifest["oil paintings"]
 end
 
+def adding
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
+end
