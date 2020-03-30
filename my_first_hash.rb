e11bc1b98518b1cda305993c499bd775f6e72594
John Stewart <jnicklausstewart@yahoo.com>
2020-03-30 15:29:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,7 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-  shipping_manifest["muskets"] = 2 
+  shipping_manifest["muskets"] = 2, 
   
   shipping_manifest2["gun powder"] = 4
   
